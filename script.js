98729fe595607ddced53295324080ac0222eeab2
erickimai <erick.imai.rodrigues@gmail.com>
2026-03-11 20:44:02 -0300

    Add project structure

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggle(){
+
+let semanas = document.getElementById("semanas");
+
+if(semanas.style.display === "block"){
+
+semanas.style.display = "none";
+
+}else{
+
+semanas.style.display = "block";
+
+}
+
+}
